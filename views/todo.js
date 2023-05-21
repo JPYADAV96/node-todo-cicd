a62efb89dca8e4d4423268c3d38bf7cc046cd43c
JP YADAV <114406274+JPYADAV96@users.noreply.github.com>
2023-05-21 20:11:31 +0530

    Update and rename todo.ejs to todo.js

--- views/todo.js
+++ views/todo.js
@@ -14,7 +14,7 @@
     </head>
 
     <body>
-        <h1>This is Vikash Node-js-todo(DevOps wale bhaiyya)</h1>
+        <h1>This is Vikash Node-js-todo(DevOps wale bhaiyya Train with shubham londhe)</h1>
         <ul>
             <% todolist.forEach(function(todo, index) { %>
             <li>
